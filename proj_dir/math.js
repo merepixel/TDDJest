f355ebc2e107ee9a47b5db5675facf38e335e13a
merepixel <89858634+merepixel@users.noreply.github.com>
2024-02-05 17:39:05 +0800

    multiply

---
+++ proj_dir/math.js
@@ -0,0 +1,7 @@
+// math.js
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  module.exports = { multiply };
+  
---
+++ proj_dir/math.test.js
@@ -0,0 +1,5 @@
+const { multiply } = require('./math');
+
+test('multiplies 1 * 2 to equal 2', () => {
+  expect(multiply(1, 2)).toBe(2);
+});
